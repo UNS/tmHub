45c9a71fa94e860715f1212fd8f4d42abc2945d4
remal <remal@DNS>
2013-03-14 23:16:56 +0900

    profile control

--- src/main/java/net/tmhub/Control.java
+++ src/main/java/net/tmhub/Control.java
@@ -1,8 +1,11 @@
 package net.tmhub;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import net.tmhub.obj.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -17,43 +20,47 @@ public class Control {
 	@Autowired
 	Repa rep;
 
-	@RequestMapping(value = "/hello", method = RequestMethod.GET)
+	@RequestMapping(value = "/{user}", method = RequestMethod.POST) 
 	@ResponseBody
-	public Pack Hello() {
-		return new Pack();
+	public Profile createProfile(@PathVariable("user") String user, @ModelAttribute("Profile") Profile p) {
+		p.setName(user);
+		rep.saveProfile(p);
+		return p;
+	}
+	
+	@RequestMapping(value = "/{user}", method = RequestMethod.GET)
+	@ResponseBody
+	public Profile getProfile(@PathVariable("user") String user) {
+		return rep.getProfile(user);
+	}
+
+	@RequestMapping(value = "/{user}/{tm}/prog", method = RequestMethod.GET)
+	@ResponseBody
+	public Profile handler(@PathVariable("user") String user, @PathVariable("tm") String tm) {
+//		return new Ok(user + "|" + tm);
+		Profile p = new Profile();
+		p.setEmail(user + "@tmHub.net");
+		p.setLogin(user);
+		return p;
 	}
 
 	public class Ok {
 
-		String msg = "Ok";	
-	
+		long id = 1;
+		String msg = "Ok";
+
+		public Ok(String msg) {
+			this.msg = msg;
+		}
+
 		@JsonProperty
 		public String getMsg() {
 			return msg;
 		}
 
-		long id = 1;
-
 		@JsonProperty
 		public long getId() {
 			return id;
-		} 
-	}
-
-	public class Pack {
-
-		String foo;
-
-		@JsonProperty
-		public String foo() {
-			rep.saveMe();
-			return rep.helloWorld();
-		}
-		String bar = null;
-
-		@JsonProperty
-		public String bar() {
-			return bar;
 		}
 	}
 }
